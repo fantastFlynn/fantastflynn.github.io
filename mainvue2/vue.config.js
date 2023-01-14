bb787351231ca41ad17bfaa5d043bdf5ccc7d8f7
fan <1356007565@qq.com>
2023-01-14 18:51:41 +0800

    Update vue.config.js

--- mainvue2/vue.config.js
+++ mainvue2/vue.config.js
@@ -1,22 +1,27 @@
 
 const pathLib = require("path");
+const webpack = require('webpack');
 const { 
   defineConfig,
 } = require('@vue/cli-service');
 
+const packageJson = require("./package.json");
+const sourceDir = 'wbpkotpts';
+const remoteUrl = `https://justfn.github.io/${sourceDir}`;
+
 
 module.exports = defineConfig({
-  publicPath: '/wbpkotpts/mainvue2/',
-  outputDir: pathLib.resolve(__dirname, "../wbpkotpts/mainvue2/"),
+  publicPath: `/${sourceDir}/${packageJson.name}/`,
+  outputDir: pathLib.resolve(__dirname, `../${sourceDir}/${packageJson.name}/`),
   transpileDependencies: true,
   chainWebpack: (cfg)=>{
     cfg.plugin('module-federation-plugin')
-      .use(require('webpack').container.ModuleFederationPlugin, [{
+      .use(webpack.container.ModuleFederationPlugin, [{
         name: "github_web4more", 
         // filename: "mf.js",
-        remotes: { // 导入
-          remote_libs: 'remote_libs@https://justfn.github.io/wbpkotpts/remote-libs/mf.js',
-          remote_vue_applications: 'remote_vue_applications@https://justfn.github.io/wbpkotpts/remote-vue-applications/mf.js',
+        remotes: { 
+          remote_libs: `remote_libs@${remoteUrl}/remote_libs/mf.js`,
+          remote_vue_applications: `remote_vue_applications@${remoteUrl}/remote_vue_applications/mf.js`,
         },
       }]);
   },
