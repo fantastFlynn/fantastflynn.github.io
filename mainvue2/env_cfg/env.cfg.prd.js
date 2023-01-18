e87adffdb0f30f5870fa218f2e438d49c91206ac
fan <1356007565@qq.com>
2023-01-19 00:16:43 +0800

    Update env.cfg.prd.js

--- mainvue2/env_cfg/env.cfg.prd.js
+++ mainvue2/env_cfg/env.cfg.prd.js
@@ -10,6 +10,12 @@ module.exports = {
       port: 443,
       path: '/wbpkotpts',
     },
+    remote_vue_applications: {
+      protocol: 'https:',
+      hostname: `justfn.github.io`,
+      port: 443,
+      path: '/wbpkotpts',
+    },
   },
   
   // 以下为Prd环境独有字段
